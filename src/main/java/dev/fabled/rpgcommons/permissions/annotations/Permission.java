0771582fce932b794b7ff8544bdbaa9341c2a776
Mantice <79672154+DeOpping@users.noreply.github.com>
2024-12-28 14:40:41 -0500

    feat: annotation methods

--- src/main/java/dev/fabled/rpgcommons/permissions/annotations/Permission.java
+++ src/main/java/dev/fabled/rpgcommons/permissions/annotations/Permission.java
@@ -1,5 +1,8 @@
 package dev.fabled.rpgcommons.permissions.annotations;
 
+import org.bukkit.permissions.PermissionDefault;
+import org.jetbrains.annotations.NotNull;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -8,6 +11,11 @@ import java.lang.annotation.Target;
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Permission {
+
+    @NotNull String value();
+    @NotNull String description() default "";
+    @NotNull PermissionDefault permissionDefault() default PermissionDefault.FALSE;
+    @NotNull String[] children() default {""};
 
 }
 
